46fe6aa33125492aa1305cb107fd6fcd21be9d95
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-13 23:07:49 +0800

    Multiplication Table Andrey feature: create multiplication table logic that satisfy the given test cases

---
+++ PDCA.md
@@ -0,0 +1,23 @@
+getMultiplicationTable()<br>
+<ul>
+  <li>P: 5 mins</li>
+  <li>D: 30 mins</li>
+  <li>C: Experienced difficulty in formulating logic with desired output</li>
+  <li>A: Practice more on logic formulation in Java and study efficient way of coding in Java 8</li>
+</ul>
+
+validateRange()<br>
+<ul>
+    <li>P: 2 mins</li>
+    <li>D: 2 mins</li>
+    <li>C: Logic was done on time</li>
+    <li>A: Need to practice more efficient way of validation</li>
+</ul>
+
+validateStartNumEndNum()<br>
+<ul>
+    <li>P: 2 mins</li>
+    <li>D: 2 mins</li>
+    <li>C: Logic was done on time</li>
+    <li>A: Need to practice more efficient way of validation</li>
+</ul>
--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,55 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
-        return null;
+
+    public static final String multipliedBy = "*";
+    public static final String equals = "=";
+    public static final String emptyString = "";
+    public static final String doubleSpace = "  ";
+
+    public String generateMultiplicationTable(int startNum, int endNum){
+        boolean validateStartNumEndNum = validateStartNumEndNum(startNum, endNum);
+
+        boolean validateStartNumRange = validateRange(endNum);
+
+        boolean validateEndNumRange = validateRange(startNum);
+
+        boolean validateRange = validateStartNumRange && validateEndNumRange;
+
+        boolean isValid = validateStartNumEndNum && validateRange;
+
+        return getMultiplicationTable(startNum, endNum, isValid);
+    }
+
+    private String getMultiplicationTable(int startNum, int endNum, boolean isValid) {
+        String result = null;
+        if(isValid){
+            for (int secondNumber = startNum; secondNumber < endNum +1; secondNumber++) {
+                for(int firstNumber = startNum; firstNumber <= secondNumber; firstNumber++){
+                    result += firstNumber + multipliedBy + secondNumber + equals +(firstNumber*secondNumber);
+                    result += secondNumber==firstNumber ? emptyString : doubleSpace;
+                }
+                if(secondNumber<endNum){
+                    result += "\r\n";
+                }
+            }
+            result = result.trim();
+        }
+        return result;
+    }
+
+    private boolean validateRange(int num) {
+        if((num >= 1) && num <= 1000){
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean validateStartNumEndNum(int startNum, int endNum) {
+        if(startNum <= endNum){
+            return true;
+        }
+        return false;
     }
 }
